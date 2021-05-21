f4f8c8668faceea85b87f565167df3267c10a310
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-20 21:31:12 -0500

    Add files via upload

---
+++ FuncionPrimitiva18_AdolfoFloresHernandez.m
@@ -0,0 +1,10 @@
+%Titulo : Funciones PRIMITIVAS
+% Descripción: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS
+	% Autor:Adolfo Flores Hernandez ah7942469@gmail.com
+	% Fecha: 20/05/2021
+ 
+ pkg load symbolic
+clear     
+clc
+ syms u
+int (3*(u^5)-2*(u^3))
