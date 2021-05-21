e66cb1fa8ce710d7fe46a2fdbc55faf008774f6e
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-20 21:31:12 -0500

    Add files via upload

---
+++ FuncionPrimitiva14_AdolfoFloresHernandez.m
@@ -0,0 +1,10 @@
+%Titulo : Funciones PRIMITIVAS
+% Descripción: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS
+	% Autor:Adolfo Flores Hernandez ah7942469@gmail.com
+	% Fecha: 20/05/2021
+ 
+ pkg load symbolic
+clear     
+clc
+ syms x
+int (2+(x^7))
---
+++ FuncionPrimitiva3_AdolfoFloresHernandez.m
@@ -0,0 +1,6 @@
+pkg load symbolic
+clear     
+clc
+ syms x
+ int (8*(x^2)-3*(x^3))
+ 
